176ce78b8881c10edba8bfa2aa700b078eb81aa8
YounessFTN <youness.fatine1@gmail.com>
2024-10-10 12:08:29 +0200

    Enregistrer les modifications avant de tirer

---
+++ src/home.jsx
@@ -0,0 +1,19 @@
+import { Ex1 } from "./components/exercices/cart";
+
+export function Home() {
+  return (
+    <>
+      <section className="p-2 flex-wrap flex gap-5 justify-center">
+        <Ex1 />
+        <Ex1 />
+        <Ex1 />
+        <Ex1 />
+        <Ex1 />
+        <Ex1 />
+        <Ex1 />
+        <Ex1 />
+        <Ex1 />
+      </section>
+    </>
+  );
+}
---
+++ src/router.jsx
@@ -0,0 +1,9 @@
+import { Home } from "./home.jsx";
+
+export default function Router() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
